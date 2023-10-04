f8a4d142cbc6b1766ef99b35fa942a4f9b821797
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2023-10-04 18:46:36 +0900

    add ToHtml and ToHtml Test

--- McMotdParser/MotdParser.cs
+++ McMotdParser/MotdParser.cs
@@ -13,21 +13,35 @@ namespace McMotdParser
     public class MotdParser
     {
 
-
-        //inconstruct
-        public string ToHtml(string RawMotd)
-        {
-            
-            return string.Empty;
-        }
-
+        
+        
         public List<MotdContent> ToTest(string RawMotd)
         {
+            
             RawMotd = StringUtils.EscapeCharacterReplace(RawMotd);
             RawMotd = StringUtils.ToJsonObjectString(RawMotd);
             return this.deserialize(RawMotd);
         }
-
+        //inconstruct
+        //TOOD: Adding Style
+        public string ToHtml(string RawMotd)
+        {
+            RawMotd = StringUtils.ToJsonObjectString(RawMotd);
+            var contents = this.deserialize(RawMotd);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<div class=""mcmotd-container"">");
+            foreach (var content in contents)
+            {
+                sb.Append("<span");
+                sb.Append($" style=\"color : {content.Color}\"");
+                sb.Append(">");
+                if (content.LineBreak) sb.Append("<br/>");
+                sb.Append(content.Text.Replace(" ","&nbsp;" ));
+                sb.Append("</span>");
+            }
+            sb.Append("</div>");
+            return sb.ToString();
+        }
         public List<MotdContent> deserialize(string RawMotd){
             var option = new JsonSerializerOptions();
             option.Converters.Add(new MotdDeserializer());
